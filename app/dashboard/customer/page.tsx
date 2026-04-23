0bf3ce18626a18d725c3f22901be9f1849718ca3
Ingole712521 <nehal712521@gmail.com>
2026-04-23 15:29:52 +0530

    remove few line

--- app/dashboard/customer/page.tsx
+++ app/dashboard/customer/page.tsx
@@ -2,7 +2,7 @@ export default function Customerpage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1>
-                this is Customer Page
+                this is Customer
             </h1>
         </div>
     )
